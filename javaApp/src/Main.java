4a0a44a0cdecf04d92946bc64436ccaf0dfa1476
= <=>
2014-12-06 13:20:45 +0800

    hello world

--- javaApp/src/Main.java
+++ javaApp/src/Main.java
@@ -1,15 +1,9 @@
 /**
  * Created by gddg on 2014/12/6.
  */
-
 public class Main {
-
     public static void main(String []args){
-
         System.out.printf("%s","this is apple!");
-
-
-
     }
 
 }
